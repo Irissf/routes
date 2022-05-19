4bae9f60c6c20c64784070c6a558ef3a9dee8348
Irissf <iris.seijo@gmail.com>
2022-05-19 12:36:13 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <div className="App">
+        <Navbar></Navbar>
         <h1>APP</h1>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
-        
+        <Link to="/">Inicio</Link>
+        <Link to="/contactos">Contactos</Link>
+        <Link to="/blog">Blog</Link>
     </nav>
   )
 }
